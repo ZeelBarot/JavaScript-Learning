bf7d74e18953e542e3c3f03686c3e1efdf569e58
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-20 13:37:13 +0530

    Operation added

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -25,5 +25,45 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 let someNumber = 30
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ********************************OPERATIONS**********************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+
+let str1 = "Hello"
+let str2 = " World"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3 );
+
+// console.log(true);      // => true
+// console.log(+true);     // => 1
+// console.log(+"");       // => 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
