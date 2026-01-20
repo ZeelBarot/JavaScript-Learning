55559d368720ec2f948df317f44e7020436fd800
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-20 12:49:29 +0530

    Conversion of datatypes

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,9 +1,29 @@
-let score = "33abc"
+let score = "Zeel"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+// console.log(typeof score);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1  false => 0
+
+let isLoggedIn = "Zeel"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanLoggedIn);
+
+// 1 => true   0 => false
+// "" => false
+// "Zeel" => true
+
+let someNumber = 30
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
