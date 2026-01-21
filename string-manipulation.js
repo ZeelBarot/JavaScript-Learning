ddc3b37a351be0ad63604775138afbac923a2c6f
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-21 11:42:06 +0530

    String Manipulation

---
+++ string-manipulation.js
@@ -0,0 +1,26 @@
+let userinput ="HELLO WORLD"
+let input = userinput.trim().toLowerCase();
+// console.log(input);
+
+let title = "Zeel Barot Test"
+let newTitle = title.toLowerCase().replaceAll(" ", "-");
+// console.log(newTitle);
+
+let user = {
+    name:"Zeel",
+    id:57
+}
+// console.log(`Hello ${user.name}, your id is ${user.id}`);
+
+
+let s = "abc";
+s[0] = "z";
+// console.log(s);
+
+let a = "hi";
+let b = a;
+b = b.toUpperCase();
+// console.log(a, b);
+
+let anotherStr = "JavaScript".slice(4, 10);
+console.log(anotherStr);
