ea0372c84642967e08f9c52a4b41f9b4b3e25ddd
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-21 11:35:24 +0530

    let, var, const differences

---
+++ 01_Basics/let-const-var.js
@@ -0,0 +1,22 @@
+// console.log(x);
+// let x = 5;
+
+
+// function test() {
+//   console.log(y);
+//   var y = 10;
+// }
+// test();
+
+
+// {
+//   const a = 1;
+//   {
+//     console.log(a);
+//   }
+// }
+
+
+const arr = [];
+arr.push(1);
+console.log(arr);
