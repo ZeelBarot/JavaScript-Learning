776c1af0dad7fc764c8dfeb9cca9f32f865c8d2a
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-24 12:11:29 +0530

    More about arrays

--- 02_Basics/02_arrays.js
+++ 02_Basics/02_arrays.js
@@ -13,3 +13,19 @@ marvel_heros.push(dc_heros);                        // change in original array
 const all_new_heros = [...marvel_heros, ...dc_heros]
 
 // console.log(all_new_heros);
+
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7 [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array);
+
+
+console.log(Array.isArray("Zeel"));
+console.log(Array.from("Zeel"));
+console.log(Array.from({name: "zeel"}));
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));
