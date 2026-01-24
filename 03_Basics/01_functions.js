cc72e69eaea45522591fd4ae0fef001a5949afb5
Zeel Barot <161916029+ZeelBarot@users.noreply.github.com>
2026-01-24 15:40:20 +0530

    functions and objects

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -27,4 +27,34 @@ function loginUserMessage(username){
 }
 
 // console.log(loginUserMessage("Zeel"));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200, 400, 500, 2000));
+
+const user = {
+    username: "zeel",
+    price: 199
+}
+
+function handleObject(anyObject){
+    console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+
+// handleObject(user)
+handleObject({
+    username: "sam",
+    price: 399
+})
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
